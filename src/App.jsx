fa5c1d8c99f6dccadf78367bd77ccf2cfacf90bb
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2023-10-19 20:07:23 +0530

    React-Price-Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './styles/App.css';
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const thirdButtonClick = () => {
+    alert('clicked!');
+  };
+  return(
+    <div className="price-cards-container">
+      <div className="price-card">
+        <p id="grey1">Free</p>
+        <h1>$0/month</h1>
+        <hr/>
+        <p>&#x2713; Single User</p>
+        <p>&#x2713; 50GB Storage</p>
+        <p>&#x2713; Unlimited Public Projects</p>
+        <p>&#x2713; Community Access</p>
+        <p id="grey">&#88; Unlimited Private Projects</p>
+        <p id="grey">&#88; Dedicated Phone Support</p>
+        <p id="grey">&#88; Free Subdomain</p>
+        <p id="grey">&#88; Monthly Status Reports</p>
+        <button disabled>BUTTON</button>
+      </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="price-card">
+        <p id="grey1">PLUS</p>
+        <h1>$9/month</h1>
+        <hr/>
+        <p>&#x2713; 5 Users</p>
+        <p>&#x2713; 50GB Storage</p>
+        <p>&#x2713; Unlimited Public Project</p>
+        <p>&#x2713; Community Access</p>
+        <p>&#x2713; Unlimited Private Project</p>
+        <p>&#x2713; Dedicated Phone Support</p>
+        <p>&#x2713; Free Subdomain</p>
+        <p id="grey">&#88; Monthly Status Reports</p>
+        <button disabled>BUTTON</button>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="price-card">
+        <p id="grey1">PRO</p>
+        <h1>$49/month</h1>
+        <hr/>
+        <p>&#x2713; Unlimited Users</p>
+        <p>&#x2713; 50GB Storage</p>
+        <p>&#x2713; Unlimited Public Project</p>
+        <p>&#x2713; Community Access</p>
+        <p>&#x2713; Unlimited Private Project</p>
+        <p>&#x2713; Dedicated Phone Support</p>
+        <p>&#x2713; Free Subdomain</p>
+        <p>&#x2713; Monthly Status Reports</p>
+        <button>BUTTON</button>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
